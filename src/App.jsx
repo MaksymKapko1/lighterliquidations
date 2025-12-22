4c9f6e594d1bc569381c884b6fe01a3dca4c608c
Максим <maksym@MacBook-Air-Maksim.local>
2025-12-22 18:13:41 +0200

    snow add

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
 import { CoinLiquidationPage } from "./Pages/CoinLiquidationPage";
+import { Snowfall } from "react-snowfall";
 import { Link } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
     <Router>
+      <Snowfall
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 9999,
+          pointerEvents: "none",
+        }}
+        snowflakeCount={60}
+        radius={[0.3, 2.0]}
+      />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/liquidations/:coin" element={<CoinLiquidationPage />} />
