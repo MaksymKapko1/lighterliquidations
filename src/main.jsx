9175ea05ba5c11c4d28b4893021a7132a53db9b9
maksym <kapkomaxim@gmail.com>
2026-02-27 11:19:08 +0200

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import App from "./App.jsx";
 import "antd/dist/reset.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-X3X3GMD6F4");
+
+ReactGA.send("pageview");
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
